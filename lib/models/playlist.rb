bdef2300ec77cd39fd82586d6dacd99bf0781333
tuckerTU <tuckerishere@gmail.com>
2020-12-08 12:30:11 -0700

    Build accessor, class variable, & initialize method

--- lib/models/playlist.rb
+++ lib/models/playlist.rb
@@ -1,3 +1,13 @@
 class Playlist
-    
+    attr_accessor :name, :id
+    @@all = []
+
+    def self.all 
+        @@all 
+    end
+
+    def initialize(name, id)
+        @name = name
+        @id = id 
+    end
 end
