a81f7971f148cf6dba9d3a7ffe4a03a53fd05f10
tuckerTU <tuckerishere@gmail.com>
2020-12-13 14:20:18 -0700

    fix the artist_name variable in the return random song method

--- lib/models/rspotify.rb
+++ lib/models/rspotify.rb
@@ -10,9 +10,9 @@ class Rspotify
     def return_random_song(username, playlist_id) 
         playlist = RSpotify::Playlist.find(username, playlist_id)
         random = rand(playlist.tracks.length)
-        @artist_name = playlist.tracks[random].artists[0].name
+        artist_name = playlist.tracks[random].artists[0].name
         song_name = playlist.tracks[random].name
-        puts "#{@artist_name}- \"#{song_name}\"" 
+        puts "#{artist_name}- \"#{song_name}\"" 
     end
 
 end
